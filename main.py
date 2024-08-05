e0d10578ed50de3488317345082149512c5de8a9
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2024-08-05 20:46:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from machine import Pin
 from utime import sleep
+from picozero import RGBLED
 
 
 print("Hello, Pi Pico!")
 
 led = Pin(5, Pin.OUT)
+
+rgb = RGBLED(blue = 20, red = 19, green = 18)
+
 ldr = machine.ADC(27)
+
+rgb.color = (0, 0, 0)
+
 while True:
     resultdata = ldr.read_u16()
     print(resultdata)
@@ -14,6 +21,3 @@ while True:
     else:
         led.on()
     sleep(0.5)
-  
-
- 
