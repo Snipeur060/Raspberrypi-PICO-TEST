f492fb3b00bd8e1192ca463b51df882c9ef73c89
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2024-08-14 12:54:52 +0200

    Update main.py

--- flaskserver/main.py
+++ flaskserver/main.py
@@ -1,2 +1,19 @@
-print("hello world")
-prti("cc")
+from flask import Flask
+#html render
+from flask import render_template
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    print("Flask APP is running")
+    app.run(debug=True)
+
